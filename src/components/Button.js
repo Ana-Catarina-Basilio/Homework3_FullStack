73d29e355fd8cd5dfa1317f06d9b328dfe299132
Ana Catarina Basílio <82106354+Ana-Catarina-Basilio@users.noreply.github.com>
2023-12-05 19:54:32 +0000

    Add components - Button
    Adding a login button to the web app.  The .css file will define the button style.

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+// src/components/Button.js
+import React from 'react';
+import './Button.css';
+
+const Button = ({ onClick, children }) => {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
