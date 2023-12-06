9fb853eb6656bb5148a38df5fb41a03a5dc73de2
Ana Catarina Basílio <82106354+Ana-Catarina-Basilio@users.noreply.github.com>
2023-12-06 14:27:35 +0000

    Update App.js
    Update App.js to include the route paths for all the components created.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import Welcome from './components/Welcome';
+import LoginError from './components/LoginError';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/login-error" element={<LoginError />} />
       </Routes>
     </Router>
   );
